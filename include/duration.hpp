0d676ccbbf709039ab08c6e2b9d1a5a3d680b99f
Teun0n <simon.a.andreasen@gmail.com>
2022-11-28 14:18:10 +0100

    work in progress

--- include/duration.hpp
+++ include/duration.hpp
@@ -1,16 +1,15 @@
 #ifdef _DURATION_H_
 #define _DURATION_H_
-#include <stdbool.h>
 
 class duration
 {
     private:
         int time;
-        
     public:
         Duration();//constructur
         Duration(int t);//constructur where time=t
         ~Duration();//deconstructer
         getDuration();//returns time
+        SetAlarm(int t);
 };
 #endif //duration_h
